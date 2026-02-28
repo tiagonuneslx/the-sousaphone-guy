00f74e589f7dc6bf86a483b02d9f14e1fc42dd0c
Tiago Nunes <tiago.nunes.lx@gmail.com>
2026-02-28 23:51:50 +0000

    Fixed ragdoll torque

--- ragdoll_spawner.gd
+++ ragdoll_spawner.gd
@@ -1,15 +1,28 @@
 extends Node2D
+class_name RagdollSpawner
 
-@onready var sprite: Sprite2D = $Sprite2D
-@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@export_custom(PROPERTY_HINT_NONE, "suffix:kg")
+var inertia: float = 0.0
+@export_custom(PROPERTY_HINT_NONE, "suffix:kg")
+var mass: float = 1.0
+
+var sprite: Sprite2D = null
+var collision_shape: CollisionShape2D = null
+
+
+func setup(p_sprite: Sprite2D, p_collision_shape: CollisionShape2D) -> void:
+	sprite = p_sprite
+	collision_shape = p_collision_shape
 
 
 func spawn_ragdoll() -> void:
-	var container: Node2D = Global.get_game().obstacle_spawner.obstacles_container
 	print_debug(get_parent().name + ".spawn_ragdoll()")
+	var container: Node2D = Global.get_game().obstacle_spawner.obstacles_container
 	var ragdoll = RigidBody2D.new()
 	ragdoll.collision_layer = 0
 	ragdoll.collision_mask = 0
+	ragdoll.inertia = inertia
+	ragdoll.mass = mass
 	ragdoll.set_collision_mask_value(1, true)
 	ragdoll.global_position = get_parent().global_position
 	ragdoll.z_index = get_parent().z_index
@@ -19,6 +32,6 @@ func spawn_ragdoll() -> void:
 	ragdoll.add_child(sprite.duplicate())
 	ragdoll.add_child(collision_shape.duplicate())
 	(func(): container.add_child(ragdoll)).call_deferred()
-	var impulse_direction = Vector2.from_angle(-PI / 4)
-	var impulse_vector = impulse_direction * 200
-	ragdoll.apply_impulse(impulse_vector)
+	var impulse_direction = Vector2.from_angle(-PI / 3)
+	var impulse_vector = impulse_direction * 300
+	ragdoll.apply_impulse(impulse_vector, Vector2(-8, 0))
